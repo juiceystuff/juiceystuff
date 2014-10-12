1f16156000cee69bbbf536e10175bd3d963efbda
juiceystuff <jengarweb@gmail.com>
2014-10-12 12:37:07 +0100

    Test

--- index.php
+++ index.php
@@ -117,7 +117,7 @@
 </section>
 
 
-<section id="panel2">				
+<section id="panel12">				
 	<div class="row">
         	<div class="twelve columns centered text-center">
         		<div class="six columns" id="strategic">
